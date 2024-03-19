38d03321655adcb7b1632e223e3d1a75c686dccb
electrovir <electrovir@users.noreply.github.com>
2024-03-19 01:15:48 +0000

    add cannabis, cocoa, coffee, cotton, dye, grape, and tulip

--- src/services/calculator/crops.ts
+++ src/services/calculator/crops.ts
@@ -4,22 +4,54 @@ export type Crop = {
     name: string;
     yield: number;
     fertilityDependence: number;
+    isFood?: true | undefined;
 };
 
 const cropDefinitions = {
     Wheat: {
         yield: 72,
         fertilityDependence: 90,
+        isFood: true,
     },
     'Medicinal Herb': {
         fertilityDependence: 90,
         yield: 94,
     },
-    // Rice: {},
     Cabbage: {
         yield: 65,
         fertilityDependence: 70,
+        isFood: true,
     },
+    Cannabis: {
+        yield: 84,
+        fertilityDependence: 70,
+    },
+    Cocoa: {
+        yield: 94,
+        fertilityDependence: 90,
+    },
+    Coffee: {
+        yield: 94,
+        fertilityDependence: 90,
+    },
+    Cotton: {
+        yield: 84,
+        fertilityDependence: 70,
+    },
+    Dye: {
+        yield: 84,
+        fertilityDependence: 70,
+    },
+    Grape: {
+        yield: 72,
+        fertilityDependence: 90,
+        isFood: true,
+    },
+    Tulip: {
+        yield: 94,
+        fertilityDependence: 90,
+    },
+    // Rice: {},
     // Melon: {},
     // Blueberry: {},
     // Corn: {},
